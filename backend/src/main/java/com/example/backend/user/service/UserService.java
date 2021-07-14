e74be1bf31d60b9add797b485f2f6150ad921b72
Urs Alexandru-Tiberiu <chictor734@gmail.com>
2021-07-14 10:53:04 +0300

    Added an exception handler

--- backend/src/main/java/com/example/backend/user/service/UserService.java
+++ backend/src/main/java/com/example/backend/user/service/UserService.java
@@ -9,7 +9,6 @@ import com.example.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityNotFoundException;
